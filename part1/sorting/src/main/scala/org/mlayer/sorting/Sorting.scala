944f2aaaa0ae8f9f6aa2ff6bc0363670b98b5d05
mdv <melnikovdv@gmail.com>
2015-02-02 12:50:04 +0300

    productivity issues fixed for merge sort and iversions count

--- part1/sorting/src/main/scala/org/mlayer/sorting/Sorting.scala
+++ part1/sorting/src/main/scala/org/mlayer/sorting/Sorting.scala
@@ -12,38 +12,44 @@ object Sorting {
   }
 
   def sortByMerge(list: List[Int]): List[Int] = {
-    val n = list.length / 2
-    if (n == 0) list
-    else {
-      def merge(fst: List[Int], scd: List[Int], acc: List[Int] = List()): List[Int] = (fst, scd) match {
-        case (Nil, ys) => acc ::: ys
-        case (xs, Nil) => acc ::: xs
-        case (x :: xs1, y :: ys1) =>
-          if (x < y) merge(xs1, scd, acc :+ x)
-          else merge(fst, ys1, acc :+ y)
+    def doSort(list: List[Int], len: Int): List[Int] = {
+      val n = len / 2
+      if (n == 0) list
+      else {
+        def merge(fst: List[Int], scd: List[Int], acc: List[Int] = List()): List[Int] = (fst, scd) match {
+          case (Nil, ys) => acc.reverse ::: ys
+          case (xs, Nil) => acc.reverse ::: xs
+          case (x :: xs1, y :: ys1) =>
+            if (x < y) merge(xs1, scd, x :: acc)
+            else merge(fst, ys1, y :: acc)
+        }
+        val (fst, scd) = list.splitAt(n)
+        merge(doSort(fst, n), doSort(scd, len - n))
       }
-      val (fst, scd) = list.splitAt(n)
-      merge(sortByMerge(fst), sortByMerge(scd))
     }
+    doSort(list, list.length)
   }
 
-  def countInversions(list: List[Int]): (List[Int], Long) = {
-    val n = list.length / 2
-    if (n == 0) (list, 0L)
-    else {
-      def mergeAndCountSplit(fst: List[Int], scd: List[Int], accInv: Long = 0L, acc: List[Int] = List()): (List[Int], Long) =
-        (fst, scd) match {
-        case (Nil, ys) => (acc ::: ys, accInv)
-        case (xs, Nil) => (acc ::: xs, accInv)
-        case (x :: xs1, y :: ys1) =>
-          if (x <= y) mergeAndCountSplit(xs1, scd, accInv, acc :+ x)
-          else mergeAndCountSplit(fst, ys1, accInv + 1 + xs1.length, acc :+ y)
+  def countInversions(list: List[Int]): Long = {
+    def doCount(list: List[Int], len: Int): (List[Int], Long) = {
+      val n = len / 2
+      if (n == 0) (list, 0L)
+      else {
+        def mergeAndCountSplit(fst: List[Int], fstLen: Int, scd: List[Int], scdLen: Int, accInv: Long = 0L, acc: List[Int] = List()): (List[Int], Long) =
+          (fst, scd) match {
+            case (Nil, ys) => (acc.reverse ::: ys, accInv)
+            case (xs, Nil) => (acc.reverse ::: xs, accInv)
+            case (x :: xs1, y :: ys1) =>
+              if (x <= y) mergeAndCountSplit(xs1, fstLen - 1, scd, scdLen, accInv, x :: acc)
+              else mergeAndCountSplit(fst, fstLen, ys1, scdLen - 1, accInv + 1 + fstLen - 1, y :: acc)
+          }
+        val (fst, scd) = list.splitAt(n)
+        val (left, countFst) = doCount(fst, n)
+        val (right, countScd) = doCount(scd, len - n)
+        mergeAndCountSplit(left, n, right, len - n, countFst + countScd)
       }
-      val (fst, scd) = list.splitAt(n)
-      val (left, countFst) = countInversions(fst)
-      val (right, countScd) = countInversions(scd)
-      mergeAndCountSplit(left, right, countFst + countScd)
     }
+    doCount(list, list.length)._2
   }
 
   def bubbleSort(list: List[Int]): List[Int] = {
--- part1/sorting/src/test/scala/org/mlayer/sorting/Sorting$Test.scala
+++ part1/sorting/src/test/scala/org/mlayer/sorting/Sorting$Test.scala
@@ -28,26 +28,26 @@ class Sorting$Test extends FunSuite {
   }
 
   test("countInversions") {
-    assert(Sorting.countInversions(List()) === (List(), 0))
-    assert(Sorting.countInversions(List(1)) === (List(1), 0))
-    assert(Sorting.countInversions(List(1, 2)) === (List(1, 2), 0))
-    assert(Sorting.countInversions(List(2, 1)) === (List(1, 2), 1))
+    assert(Sorting.countInversions(List()) === 0)
+    assert(Sorting.countInversions(List(1)) === 0)
+    assert(Sorting.countInversions(List(1, 2)) === 0)
+    assert(Sorting.countInversions(List(2, 1)) === 1)
 
-    assert(Sorting.countInversions(List(1, 3, 5, 2, 4, 6)) === (List(1, 2, 3, 4, 5, 6), 3))
-    assert(Sorting.countInversions(List(1, 5, 3, 2, 4))._2 === 4)
-    assert(Sorting.countInversions(List(5, 4, 3, 2, 1))._2 === 10)
-    assert(Sorting.countInversions(List(1, 3, 1))._2 === 1)
-    assert(Sorting.countInversions(List(1, 1, 1))._2 === 0)
-    assert(Sorting.countInversions(List(1, 3, 5, 2, 4, 6, 1))._2 === 8)
-    assert(Sorting.countInversions(List(9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0))._2 === 56)
+    assert(Sorting.countInversions(List(1, 3, 5, 2, 4, 6)) === 3)
+    assert(Sorting.countInversions(List(1, 5, 3, 2, 4)) === 4)
+    assert(Sorting.countInversions(List(5, 4, 3, 2, 1)) === 10)
+    assert(Sorting.countInversions(List(1, 3, 1)) === 1)
+    assert(Sorting.countInversions(List(1, 1, 1)) === 0)
+    assert(Sorting.countInversions(List(1, 3, 5, 2, 4, 6, 1)) === 8)
+    assert(Sorting.countInversions(List(9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0)) === 56)
     assert(Sorting.countInversions(List(37, 7, 2, 14, 35, 47, 10, 24, 44, 17, 34, 11, 16, 48, 1,
       39, 6, 33, 43, 26, 40, 4, 28, 5, 38, 41, 42, 12, 13, 21, 29, 18, 3, 19, 0, 32, 46, 27, 31,
-      25, 15, 36, 20, 8, 9, 49, 22, 23, 30, 45))._2 === 590)
+      25, 15, 36, 20, 8, 9, 49, 22, 23, 30, 45)) === 590)
     assert(Sorting.countInversions(List(4, 80, 70, 23, 9, 60, 68, 27, 66, 78, 12, 40, 52, 53, 44,
       8, 49, 28, 18, 46, 21, 39, 51, 7, 87, 99, 69, 62, 84, 6, 79, 67, 14, 98, 83, 0, 96, 5, 82,
       10, 26, 48, 3, 2, 15, 92, 11, 55, 63, 97, 43, 45, 81, 42, 95, 20, 25, 74, 24, 72, 91, 35,
       86, 19, 75, 58, 71, 47, 76, 59, 64, 93, 17, 50, 56, 94, 90, 89, 32, 37, 34, 65, 1, 73, 41,
-      36, 57, 77, 30, 22, 13, 29, 38, 16, 88, 61, 31, 85, 33, 54))._2 === 2372)
+      36, 57, 77, 30, 22, 13, 29, 38, 16, 88, 61, 31, 85, 33, 54)) === 2372)
   }
 
   test("bubbleSort test") {
@@ -56,18 +56,22 @@ class Sorting$Test extends FunSuite {
     assert(Sorting.isSorted(sorted))
   }
 
-  ignore("count time for merge sorting and bubble sort") {
-    val a = Sorting.genList(5000, 1000)
-
-    var start = Platform.currentTime
+  test("merge sort time") {
+    val a = Sorting.genList(100000, 10000)
+    val start = Platform.currentTime
     val a1 = Sorting.sortByMerge(a)
-    println("Time for merge sort size = " + a.length + " is: " + (Platform.currentTime - start) + " ms")
-    start = Platform.currentTime
-    val a2 = Sorting.bubbleSort(a)
-    println("Time for buuble sort size = " + a.length + " is: " + (Platform.currentTime - start) + " ms")
+    println("Merge sort time for array size = " + a.length + " is: " + (Platform.currentTime - start) + " ms")
+
   }
 
-  ignore("assigment 1: count array inversions") {
+  ignore("time for bubble sorting") {
+    val a = Sorting.genList(5000, 1000)
+    val start = Platform.currentTime
+    val sorted = Sorting.bubbleSort(a)
+    println("Time for buuble sort size = " + sorted.length + " is: " + (Platform.currentTime - start) + " ms")
+  }
+
+  test("assigment 1: count array inversions") {
     def loadDictionary(file: String): List[Int] = {
       val wordstream = Option {
         getClass.getClassLoader.getResourceAsStream(file)
@@ -86,12 +90,9 @@ class Sorting$Test extends FunSuite {
       }
     }
 
-
+    val start = Platform.currentTime
     val list = loadDictionary("IntegerArray.txt")
-    println("array length: " + list.length)
-    println("first 10 elements: " + list.dropRight(list.length - 9).mkString(" "))
-    println("inversions count: " + Sorting.countInversions(list)._2)
-
+    println("\ninversions count for array size " + list.length + " is: " + Sorting.countInversions(list) + ". Time: " + (Platform.currentTime - start) + " ms")
   }
 
 }
